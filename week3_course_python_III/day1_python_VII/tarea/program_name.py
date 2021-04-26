7da191a2843e9c988bc766aaa707afe98c7aba9d
JoseKuru <josecarlosbatistarivero@gmail.com>
2021-04-26 21:05:49 +0200

    Añadido los guiones de arriba

--- week3_course_python_III/day1_python_VII/tarea/program_name.py
+++ week3_course_python_III/day1_python_VII/tarea/program_name.py
@@ -1,3 +1,3 @@
-board = [['|' if i%2==0 else '_' for i in range(17)] for i in range(8)]
+board = [['|' if i%2==0 and x!=0 else ' ' if i%2==0 and x==0 else '_' for i in range(17)] for x in range(9)]
 for row in board:
     print(''.join(row))
