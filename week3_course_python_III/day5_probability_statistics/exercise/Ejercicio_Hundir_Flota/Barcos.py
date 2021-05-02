57baf327963bd0ac8b994e022d04ce542594f53a
JoseKuru <josecarlosbatistarivero@gmail.com>
2021-05-02 23:41:28 +0200

    Añadido funcionalidades al ejercicio de hundir la flota

--- week3_course_python_III/day5_probability_statistics/exercise/Ejercicio_Hundir_Flota/Barcos.py
+++ week3_course_python_III/day5_probability_statistics/exercise/Ejercicio_Hundir_Flota/Barcos.py
@@ -9,16 +9,24 @@ class Barcos:
         self.vida = largo
 
 
-    def colocar_barco(self, coordenada_y, coordenada_x, Tablero):
+    def colocar_barco(self, coordenada_y, coordenada_x, Tablero, lista_posiciones):
+        if coordenada_y == 0 or coordenada_y > 10 or coordenada_x == 0 or coordenada_x > 10:
+            raise Exception('Fuera de rango')
+
         if self.orientacion == 'h':
             for i in range(len(Tablero.tabla[coordenada_y])):
-                if i in [x for x in range(coordenada_x, coordenada_x + self.largo)]:
+                if i in [x for x in range(coordenada_x, coordenada_x + self.largo)]: # Igual se pude unir a la linea de arriba?
+                    if i + coordenada_x > 10 or (coordenada_y, i) in lista_posiciones or (coordenada_y, i+1) in lista_posiciones or (coordenada_y, i+2) in lista_posiciones or (coordenada_y, i+3) in lista_posiciones or (coordenada_y, i+4) in lista_posiciones: #MUY Mal
+                        self.posicion = []
+                        raise Exception('Fuera de rango/ Los barcos han chocado')                       
                     self.posicion.append((coordenada_y, i))
                     Tablero.tabla[coordenada_y][i] = '#'
         
         if self.orientacion == 'v':
             for i in range(len(Tablero.tabla)):
                 if i in [x for x in range(coordenada_y, coordenada_y + self.largo)]:
+                    if i + coordenada_y > 10:
+                        raise Exception('Fuera de rango')
                     self.posicion.append((i, coordenada_x))
                     Tablero.tabla[i][coordenada_x] = '#'         
     
--- week3_course_python_III/day5_probability_statistics/exercise/Ejercicio_Hundir_Flota/main.py
+++ week3_course_python_III/day5_probability_statistics/exercise/Ejercicio_Hundir_Flota/main.py
@@ -26,39 +26,44 @@ def preguntar(Tablero, diccionario_posiciones):
     mesa_de_juego.mostrar_pantalla()
 
 def empiece_partida():
-    primer_barco_2_1 = Barcos('Primer barco 2x1', 2)
-    segundo_barco_2_1 = Barcos('Segundo barco 2x1', 2)
-    tercer_barco_2_1 = Barcos('Tercer barco 2x1', 2)
-    cuarto_barco_2_1 = Barcos('Cuarto barco 2x1', 2)
-    primer_barco_3_1 = Barcos('Primer barco 3x1', 3)
-    segundo_barco_3_1 = Barcos('Segundo barco 3x1', 3)
-    tercer_barco_3_1 = Barcos('Tercer barco 3x1', 3)
-    primer_barco_4_1 = Barcos('Primer barco 4x1', 4)
-    segundo_barco_4_1 = Barcos('Segundo barco 4x1', 4)
-    primer_barco_5_1 = Barcos('Primer barco 5x1', 5)
-    lista_barcos = [primer_barco_2_1, segundo_barco_2_1, tercer_barco_2_1, cuarto_barco_2_1, primer_barco_3_1, segundo_barco_3_1, tercer_barco_3_1, primer_barco_4_1, segundo_barco_4_1, primer_barco_5_1]
-    loop = True
+    lista_posiciones = []
     for barco in lista_barcos:
-        while loop:
+        while True:
             try:
-                os.system('clear')
+                #os.system('clear')
                 mesa_de_juego.mostrar_pantalla()
                 input_colocacion = input(f'Introduce la posicion del {barco.nombre}: ')
                 if 'h' in input_colocacion:
                     barco.establecer_orientacion('h')
                     y_input = int((input_colocacion.split('h'))[0])
                     x_input = int(((input_colocacion.split('h'))[1].split(':'))[0])
-                    loop = False
+                    barco.colocar_barco(y_input, x_input, mesa_de_juego, lista_posiciones)
+                    lista_posiciones.append((y_input, x_input)) # Añadir a la lista para chequear que dos barcos no choquen
+                    break
                 else:
                     barco.establecer_orientacion('v')
                     y_input = int((input_colocacion.split('v'))[0])
                     x_input = int(((input_colocacion.split('v'))[1].split(':'))[0]) #Muy lioso?, Solo me importa la primera coordenada de columna_inicio : columna_final
-            except Exception:
-                print('Introduce una expresion valida')
-                loop = True
-        barco.colocar_barco(y_input, x_input, mesa_de_juego)
+                    barco.colocar_barco(y_input, x_input, mesa_de_juego, lista_posiciones)
+                    lista_posiciones.append((y_input, x_input))
+                    break
+            except Exception as err:
+                print(err)
+        
+        print(barco.posicion) # --NO
 
 mesa_de_juego = Tablero(10, 10)
+primer_barco_2_1 = Barcos('Primer barco 2x1', 2)
+segundo_barco_2_1 = Barcos('Segundo barco 2x1', 2)
+tercer_barco_2_1 = Barcos('Tercer barco 2x1', 2)
+cuarto_barco_2_1 = Barcos('Cuarto barco 2x1', 2)
+primer_barco_3_1 = Barcos('Primer barco 3x1', 3)
+segundo_barco_3_1 = Barcos('Segundo barco 3x1', 3)
+tercer_barco_3_1 = Barcos('Tercer barco 3x1', 3)
+primer_barco_4_1 = Barcos('Primer barco 4x1', 4)
+segundo_barco_4_1 = Barcos('Segundo barco 4x1', 4)
+primer_barco_5_1 = Barcos('Primer barco 5x1', 5)
+lista_barcos = [primer_barco_2_1, segundo_barco_2_1, tercer_barco_2_1, cuarto_barco_2_1, primer_barco_3_1, segundo_barco_3_1, tercer_barco_3_1, primer_barco_4_1, segundo_barco_4_1, primer_barco_5_1]
 empiece_partida()
 dict_posiciones = {barco : barco.posicion for barco in lista_barcos}
 mesa_de_juego.mostrar_pantalla()
